87f8e4bd118b8b3e1c16a0a5b4afeb23c09c29f6
PavelStojanov <pavel.stojanov@students.finki.ukim.mk>
2025-11-11 23:29:33 +0100

    12

---
+++ src/kol1/GenericFraction12/ZeroDenominatorException.java
@@ -0,0 +1,7 @@
+package kol1.GenericFraction12;
+
+public class ZeroDenominatorException extends RuntimeException {
+    public ZeroDenominatorException(String message) {
+        super(message);
+    }
+}
