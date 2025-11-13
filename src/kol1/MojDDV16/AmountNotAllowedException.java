928daac628ad6a094f10120ed45cd7c36f94b0b8
PavelStojanov <pavel.stojanov@students.finki.ukim.mk>
2025-11-13 22:12:16 +0100

    16

---
+++ src/kol1/MojDDV16/AmountNotAllowedException.java
@@ -0,0 +1,7 @@
+package kol1.MojDDV16;
+
+public class AmountNotAllowedException extends Exception {
+    public AmountNotAllowedException(int amount) {
+        super(String.format("Receipt with amount %d is not allowed to be scanned", amount));
+    }
+}
